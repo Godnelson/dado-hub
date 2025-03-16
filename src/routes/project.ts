a9b75dd9922fb88966a851bae1ce2ed9634cc6b2
Godnelson <luizgatohd@gmail.com>
2025-03-16 12:03:44 -0300

    bind and unbind of tags implemented

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -5,55 +5,108 @@ export const projectRoutes = Router()
 
 const prisma = new PrismaClient()
 
-projectRoutes.get("/", async (req, res) =>{
-    try{
-        const projects = await prisma.project.findMany()
+//CRUD
+
+projectRoutes.get("/", async (req, res) => {
+    try {
+        const projects = await prisma.project.findMany({
+            include: {
+              tags: true,
+            },
+          })
         res.status(200).json(projects)
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e)
     }
 })
 
-projectRoutes.get("/:id", async (req, res) =>{
-    try{
-        const project = await prisma.project.findUnique({where:{id: Number(req.params.id)}})
+projectRoutes.get("/:id", async (req, res) => {
+    try {
+        const project = await prisma.project.findUnique({ where: { id: Number(req.params.id) }, include: {tags: true} })
         if (project != null) {
             res.status(200).json(project)
             return
         }
         res.status(404).json()
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e)
     }
 })
 
 projectRoutes.post("/", async (req, res) => {
-    try{
-        const project = await prisma.project.create({data: req.body})
+    try {
+        const project = await prisma.project.create({ data: req.body })
         res.status(201).json(project)
-    }catch(e){
+    } catch (e) {
         res.status(400).send(e)
     }
 })
 
 projectRoutes.put("/:id", async (req, res) => {
-    try{
-        const project = await prisma.project.update({where: {id: Number(req.params.id)}, data: req.body})
+    try {
+        const project = await prisma.project.update({ where: { id: Number(req.params.id) }, data: req.body, include: {tags: true} })
         res.status(200).json(project)
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e)
     }
 })
 
 projectRoutes.delete("/:id", async (req, res) => {
-    try{
-        await prisma.project.delete({where: {id: Number(req.body)}})
+    try {
+        await prisma.project.delete({ where: { id: Number(req.body) } })
         res.status(204).send()
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e)
+    }
+})
+
+//BIND TAG
+
+projectRoutes.put("/bind_tag/:id/:tagId", async (req, res) => {
+    try {
+        const project = await prisma.project.update({
+            where: {
+                id: Number(req.params.id)
+            },
+            data: {
+                tags: {
+                    create: [
+                        {
+                            tag:{
+                                connect: {id: Number(req.params.tagId)}
+                            }
+                        }
+                    ]
+                }
+            },
+            include: {tags: true}
+        })
+        res.status(200).json(project)
+    }
+    catch (e) {
+        res.status(400).send()
+    }
+})
+
+//UNBIND TAG
+
+projectRoutes.delete("/unbind_tag/:id/:tagId", async (req, res) => {
+
+    try {
+        const project = await prisma.tagAssignedForProjects.delete({where:{
+            projectId_tagId: {
+                projectId: Number(req.params.id),
+                tagId: Number(req.params.tagId)
+            }
+        }})
+        res.status(204).json(project)
+    }
+    catch (e) {
+        console.log(e)
+        res.status(400).send()
     }
 })
