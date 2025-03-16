6ce8fadbd83cbbc1d09f1253a97ee809df70d628
Godnelson <luizgatohd@gmail.com>
2025-03-16 03:06:20 -0300

    tag crud implemented

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRoutes } from './routes/user';
 import { projectRoutes } from './routes/project';
+import { tagRoutes } from './routes/tag';
 export const router = Router()
 
 
 router.use("/user", userRoutes)
 router.use("/project", projectRoutes)
+router.use("/tag", tagRoutes)
