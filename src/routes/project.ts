03ccbd8fa972c270eb671b93015fcf41d0c3fc08
Godnelson <luizgatohd@gmail.com>
2025-04-16 18:57:19 -0300

    remove banner feat implemented

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -15,9 +15,9 @@ projectRoutes.get("/", async (req, res) => {
     try {
         const projects = await prisma.project.findMany({
             include: {
-              tags: true,
+                tags: true,
             },
-          })
+        })
         res.status(200).json(projects)
     }
     catch (e) {
@@ -27,7 +27,7 @@ projectRoutes.get("/", async (req, res) => {
 
 projectRoutes.get("/:id", async (req, res) => {
     try {
-        const project = await prisma.project.findUnique({ where: { id: Number(req.params.id) }, include: {tags: true} })
+        const project = await prisma.project.findUnique({ where: { id: Number(req.params.id) }, include: { tags: true } })
         if (project != null) {
             res.status(200).json(project)
             return
@@ -50,7 +50,7 @@ projectRoutes.post("/", async (req, res) => {
 
 projectRoutes.put("/:id", async (req, res) => {
     try {
-        const project = await prisma.project.update({ where: { id: Number(req.params.id) }, data: req.body, include: {tags: true} })
+        const project = await prisma.project.update({ where: { id: Number(req.params.id) }, data: req.body, include: { tags: true } })
         res.status(200).json(project)
     }
     catch (e) {
@@ -80,52 +80,65 @@ projectRoutes.put("/bind_tag/:id/:tagId", async (req, res) => {
                 tags: {
                     create: [
                         {
-                            tag:{
-                                connect: {id: Number(req.params.tagId)}
+                            tag: {
+                                connect: { id: Number(req.params.tagId) }
                             }
                         }
                     ]
                 }
             },
-            include: {tags: true}
+            include: { tags: true }
         })
         res.status(200).json(project)
     }
     catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
     }
 })
 
 //UNBIND TAG
 
-projectRoutes.delete("/unbind_tag/:id/:tagId", async (req, res) => {
+projectRoutes.put("/unbind_tag/:id/:tagId", async (req, res) => {
 
     try {
-        const project = await prisma.tagAssignedForProjects.delete({where:{
-            projectId_tagId: {
-                projectId: Number(req.params.id),
-                tagId: Number(req.params.tagId)
+        const project = await prisma.tagAssignedForProjects.delete({
+            where: {
+                projectId_tagId: {
+                    projectId: Number(req.params.id),
+                    tagId: Number(req.params.tagId)
+                }
             }
-        }})
-        res.status(204).json(project)
+        })
+        res.status(200).json(project)
     }
     catch (e) {
         console.log(e)
-        res.status(400).send()
+        res.status(400).send(e)
     }
 })
 
 //UPLOAD BANNER
 
-projectRoutes.post("/upload_banner", upload.single("banner") , async (req, res) =>{
-    try{
-        if(!req.file) throw Error("There's no file uploaded with key 'banner'")
-        if(!req.body.projectId) throw Error("There's no projectId on request body")
-        if(req.file.mimetype.split("/")[0] != "image") throw Error("'banner' is not a image")
+projectRoutes.post("/upload_banner", upload.single("banner"), async (req, res) => {
+    try {
+        if (!req.file) throw Error("There's no file uploaded with key 'banner'")
+        if (!req.body.projectId) throw Error("There's no projectId on request body")
+        if (req.file.mimetype.split("/")[0] != "image") throw Error("'banner' is not a image")
         const filePath = await uploadFile(req.file?.buffer, `/banners/${req.body.projectId}_banner.${req.file.mimetype.split("/")[1]}`)
-        const project = await prisma.project.update({where:{id: Number(req.body.projectId)}, data:{banner: filePath}})
+        const project = await prisma.project.update({ where: { id: Number(req.body.projectId) }, data: { banner: filePath } })
         res.status(200).json(project)
-    }catch(e){
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//REMOVE BANNER
+
+projectRoutes.put("/remove_banner/:id", async (req, res) => {
+    try {
+        const project = await prisma.project.update({ where: { id: Number(req.params.id) }, data: { banner: null } })
+        res.status(200).json(project)
+    } catch (e) {
         res.status(400).send(e)
     }
 })
