30103f570eda0ba4f2a83d1f130e0ce297a372f5
Luiz Amélio <68659027+Godnelson@users.noreply.github.com>
2025-05-08 20:09:57 -0300

    tailwind setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='w-full bg-[#CD9FCC]'>
+        <div className='flex gap-1 items-center'>
+          <img src="/dado_hub_logo.png" alt="dado-logo" height={78} width={68}/>
+          <div className='text-[32px]'>
+            Dado Hub
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
